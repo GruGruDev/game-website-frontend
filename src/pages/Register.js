cd6f0817d31d1af3247eafb9359f4b09f08064f6
GruGruDev <nguyentritai210804@gmail.com>
2024-10-08 15:44:33 +0700

    fix can't register or log in

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"; // Import Link từ React Router
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -11,6 +11,7 @@ const Register = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
+
     try {
       await axios.post("http://localhost:5000/api/auth/register", {
         email,
@@ -18,7 +19,7 @@ const Register = () => {
         password,
       });
       alert("Registration successful! Please login.");
-      navigate("/login"); // Điều hướng về trang login sau khi đăng ký thành công
+      navigate("/login"); // Điều hướng tới trang login sau khi đăng ký thành công
     } catch (err) {
       setError("Registration failed. Please try again.");
     }
@@ -26,61 +27,43 @@ const Register = () => {
 
   return (
     <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card shadow-lg">
-            <div className="card-body">
-              <h2 className="text-center mb-4">Register</h2>
-              <form onSubmit={handleRegister}>
-                <div className="form-group">
-                  <label>Email address</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="form-group mt-3">
-                  <label>Username</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-                <div className="form-group mt-3">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                {error && <p className="text-danger mt-3">{error}</p>}
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-block mt-4"
-                >
-                  Register
-                </button>
-              </form>
-
-              <div className="text-center mt-4">
-                <p>
-                  Already have an account? <Link to="/login">Login here</Link>
-                </p>{" "}
-                {/* Thêm liên kết đến trang Login */}
-              </div>
-            </div>
-          </div>
+      <h2 className="mb-4">Register</h2>
+      <form onSubmit={handleRegister}>
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-      </div>
+        <div className="form-group mt-3">
+          <label>Username</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group mt-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary btn-block mt-4">
+          Register
+        </button>
+        {error && <p className="text-danger mt-3">{error}</p>}
+      </form>
     </div>
   );
 };
